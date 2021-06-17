b256633b57e05a6752e2d96f85eea631430167c8
Leslie Sibanda <lesliesibanda123@gmail.com>
2021-06-17 01:02:55 +0100

    Initial Setup

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5bc5e6e0815fa007b944ce52832ab02d'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b6c4fbe9-9e88-4d0e-ac49-ea62fb477dc4'
+};
